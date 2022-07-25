89d4eab6bd8b35a4f070afad99dfa39d6f691128
Benen Kim <benen.k18@gmail.com>
2022-07-25 12:18:52 -0700

    evaluate, clear, and previous input added

--- calculator.js
+++ calculator.js
@@ -2,9 +2,11 @@ const numberBtn = document.querySelectorAll('[data-number]');
 const operatorBtn = document.querySelectorAll('[data-operator]');
 const firstOperationScreen = document.getElementById("firstOperationScreen");
 const secondOperationScreen = document.getElementById("secondOperationScreen");
+const equalsBtn = document.getElementById("equals");
+const clearBtn = document.getElementById("clear");
 
-let firstValue = firstOperationScreen.textContent;
-let secondValue = secondOperationScreen.textContent;
+let currentOperation = '';
+let resetScreen = false;
 
 numberBtn.forEach((button) => 
    button.addEventListener('click', () => appendNumber(button.textContent))
@@ -14,39 +16,52 @@ operatorBtn.forEach((button) =>
    button.addEventListener('click', () => setOperation(button.textContent))
 )
 
+equalsBtn.addEventListener('click', () => operate(currentOperation, firstOperationScreen.textContent, secondOperationScreen.textContent));
+clearBtn.addEventListener('click', () => clear());
+
 function appendNumber(number) {
-   if(secondValue === '0')
-      secondOperationScreen.textContent = "";
+   if(resetScreen) {
+      secondOperationScreen.textContent = '';
+      resetScreen = false;
+   }
    secondOperationScreen.textContent += number;
 }
 
 function setOperation(operator) {
    currentOperation = operator;
-   firstValue = secondValue;
+   firstOperationScreen.textContent = secondOperationScreen.textContent;
    secondOperationScreen.textContent = 0;
+   resetScreen = true;
+}
+
+function clear() {
+   firstOperationScreen.textContent = secondOperationScreen.textContent;
+   secondOperationScreen.textContent = 0;
+   resetScreen = true;
 }
 
 function add(a, b) {
-   let sum = a + b;
-   return sum;
+   let sum = parseInt(a) + parseInt(b);
+   secondOperationScreen.textContent = sum;
 }
 
 function subtract(a, b) {
    let difference = a - b;
-   return difference;
+   secondOperationScreen.textContent = difference;
 }
 
 function multiply(a, b) {
    let product = a * b;
-   return product;
+   secondOperationScreen.textContent = product;
 }
 
 function divide(a, b) {
    let dividend = a / b;
-   return dividend;
+   secondOperationScreen.textContent = dividend;
 }
 
 function operate(operator, a, b) {
+   resetScreen = true;
    if(operator === '+')
       add(a, b);
    else if(operator === '-')
